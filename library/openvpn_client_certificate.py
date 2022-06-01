ca2e3064dfb87d0a87312fa5cb3bcd96ccfa6977
Bodo Schulz <bodo@boone-schulz.de>
2022-06-01 06:47:53 +0200

    fix library classes

--- library/openvpn_client_certificate.py
+++ library/openvpn_client_certificate.py
@@ -5,7 +5,6 @@
 
 from __future__ import absolute_import, division, print_function
 import os
-import sys
 import hashlib
 
 from ansible.module_utils.basic import AnsibleModule
--- library/openvpn_ovpn.py
+++ library/openvpn_ovpn.py
@@ -5,6 +5,7 @@
 
 from __future__ import absolute_import, division, print_function
 import os
+import sys
 import hashlib
 
 from ansible.module_utils.basic import AnsibleModule
